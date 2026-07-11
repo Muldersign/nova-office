5c673a0158ef9699fea1a840334f465f0c59c4b3
Muldersign <212379537+Muldersign@users.noreply.github.com>
2026-07-11 08:51:01 +0200

    Use module assets for Plesk deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].mjs',
+        chunkFileNames: 'assets/[name]-[hash].mjs',
+      },
+    },
+  },
   plugins: [react()],
 })
